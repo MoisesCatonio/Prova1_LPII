ab8d511f82c2fefdb964ea60892cfd3d62cae3c5
MoisesCatonio <mocaoli@yahoo.com.br>
2018-09-02 21:10:59 -0300

    Vagas residuais sendo alocadas

--- app.py
+++ app.py
@@ -5,13 +5,25 @@ import my_lib as ml
 #Caso esteja em dúvida de alguma coisa, verifique com as exibições de listas e tuplas implementadas,
 #ml.showSomethingTupled(Dict), ml.showSomethingListed(List) ;)
 
+QE = 12684
+
+total_vagas = 29
+
 lista_geral = list(csv.reader(open('eleicao.csv', encoding="UTF-8"),delimiter=";"))
 
 coligacoes = ml.getColigacoes(lista_geral)
 
 partidos = ml.getPartidosUnitarios(lista_geral)
 
-votos_geral = ml.countVotes(partidos,coligacoes, lista_geral)
+votos_geral = ml.countVotes(partidos,coligacoes,lista_geral)
 
 #QE usado do arquivo dado pelo professor, mas função de geração de QE implementada (ml.generateQE)!!!
-QP_geral = ml.generateQP(votos_geral, 12684)
+QP_geral = ml.generateQP(votos_geral, QE)
+
+residuais = ml.defineResiduals(QP_geral, total_vagas)
+
+dict_residuals_count = dict.fromkeys(QP_geral, 0)
+
+QP_definitivo = ml.allocateResiduals(votos_geral, QP_geral, residuais, dict_residuals_count)
+
+ml.showSomethingTupled(QP_definitivo)
--- my_lib.py
+++ my_lib.py
@@ -62,9 +62,43 @@ def generateQE(dict_votos, cadeiras):
 	QE_entidades = int(sum(list(dict_votos.values()))/cadeiras)
 	return QE_entidades
 
+#Função para geração do Quociente Partidário.
+#Recebe: Dicionário, Inteiro
+#Retorna: Dicionário
 def generateQP(dict_votos, QE):
 	QP = dict.fromkeys(dict_votos, 0)
 	for coligacao in dict_votos:
-		QP[coligacao] = int(dict_votos[coligacao]/QE)
-	
+		if(int(dict_votos[coligacao]/QE) > 0):
+			QP[coligacao] = int(dict_votos[coligacao]/QE)
 	return QP
+
+#Função para geração da quantidade de vagas residuais.
+#Recebe: Dicionário, Inteiro
+#Retorna: Inteiro
+def defineResiduals(dict_QP, total_vagas):
+	return (total_vagas - int(sum(list(dict_QP.values()))))
+
+#Função para alocação de vagas residuais.
+#Recebe: Dicionário, Dicionário, Inteiro, Dicionário
+#Retorna: Dicionário
+#Tive que usar recursividade aqui T.T....
+def allocateResiduals(dict_votos, dict_QP, residuals, dict_residuals_count):
+	media = dict_votos
+	
+	for coligacao in media:
+		media[coligacao] = dict_votos[coligacao]/(dict_QP[coligacao]+dict_residuals_count[coligacao]+1)
+	
+	coef_atualizar = max(list(media.values()))
+
+	for coligacao in media:
+		if (media[coligacao] == coef_atualizar):
+			dict_QP[coligacao] += 1
+			dict_residuals_count[coligacao] += 1
+
+	residuals = residuals - 1
+	
+	while(residuals > 0):
+		return allocateResiduals(dict_votos, dict_QP, residuals, dict_residuals_count)
+
+	return dict_QP
+	
